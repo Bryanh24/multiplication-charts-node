cd90d5ee931be018382393f71d6048fa9f214ef8
Bryan Hernandez <bryanh24@hotmail.com>
2020-07-23 20:35:26 -0600

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,18 @@
+const options = {
+    base: {
+        demand: true,
+        alias: "b",
+    },
+    limite: {
+        alias: "l",
+        default: 10,
+    },
+};
+const argv = require("yargs")
+    .command("listar", "imprimie en consola la tabla de multiplicar", options)
+    .command("crear", "Crea un archivo .txt con la tabla de multiplicar", options)
+    .help().argv;
+
+module.exports = {
+    argv
+}
